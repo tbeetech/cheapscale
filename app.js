f21fd4bec611775f56ad908b739c965ceceee35f
Oluwatobi Oyebade <64794915+tbeetech@users.noreply.github.com>
2022-06-08 01:42:00 +0100

    enabled ejs middle ware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ PORT = 5000
 const express = require('express')
 const routes = require('./routes/routes')
 const app = express()
+app.set("view engine", "ejs")
 app.use('/routes/routes', routes)
 
 
